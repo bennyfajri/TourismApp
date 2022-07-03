6a4cd1771093264b8556a7f67c82549a87a98701
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-07-03 15:48:10 +0700

    call data using retrofit

--- app/src/main/java/com/drsync/tourismapp/core/di/Injection.kt
+++ app/src/main/java/com/drsync/tourismapp/core/di/Injection.kt
@@ -7,6 +7,7 @@ import com.drsync.tourismapp.core.data.source.local.room.TourismDatabase
 
 import com.drsync.tourismapp.core.data.TourismRepository
 import com.drsync.tourismapp.core.data.source.remote.RemoteDataSource
+import com.drsync.tourismapp.core.data.source.remote.network.ApiConfig
 import com.drsync.tourismapp.core.domain.repository.ITourismRepository
 import com.drsync.tourismapp.core.domain.usecase.TourismInteractor
 import com.drsync.tourismapp.core.domain.usecase.TourismUseCase
@@ -17,7 +18,7 @@ object Injection {
     fun provideRepository(context: Context): ITourismRepository {
         val database = TourismDatabase.getInstance(context)
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService())
         val localDataSource = LocalDataSource.getInstance(database.tourismDao())
         val appExecutors = AppExecutors()
 
