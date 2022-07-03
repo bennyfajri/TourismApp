e78bb1a0d0c418b5be5da4d9296b6f4dcf1839b3
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-07-04 01:56:47 +0700

    add : RxJava

--- app/src/main/java/com/drsync/tourismapp/core/domain/usecase/TourismInteractor.kt
+++ app/src/main/java/com/drsync/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -4,17 +4,14 @@ import androidx.lifecycle.LiveData
 import com.drsync.tourismapp.core.data.Resource
 import com.drsync.tourismapp.core.domain.model.Tourism
 import com.drsync.tourismapp.core.domain.repository.ITourismRepository
+import io.reactivex.Flowable
 
 class TourismInteractor(
     private val tourismRepository: ITourismRepository
 ): TourismUseCase {
-    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> = tourismRepository.getAllTourism()
+    override fun getAllTourism() = tourismRepository.getAllTourism()
 
-    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
-        return tourismRepository.getFavoriteTourism()
-    }
+    override fun getFavoriteTourism() = tourismRepository.getFavoriteTourism()
 
-    override fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
-        return tourismRepository.setFavoriteTourism(tourism, state)
-    }
+    override fun setFavoriteTourism(tourism: Tourism, state: Boolean) = tourismRepository.setFavoriteTourism(tourism, state)
 }
--- app/src/main/java/com/drsync/tourismapp/core/domain/usecase/TourismUseCase.kt
+++ app/src/main/java/com/drsync/tourismapp/core/domain/usecase/TourismUseCase.kt
@@ -3,9 +3,10 @@ package com.drsync.tourismapp.core.domain.usecase
 import androidx.lifecycle.LiveData
 import com.drsync.tourismapp.core.data.Resource
 import com.drsync.tourismapp.core.domain.model.Tourism
+import io.reactivex.Flowable
 
 interface TourismUseCase {
-    fun getAllTourism(): LiveData<Resource<List<Tourism>>>
-    fun getFavoriteTourism(): LiveData<List<Tourism>>
+    fun getAllTourism(): Flowable<Resource<List<Tourism>>>
+    fun getFavoriteTourism(): Flowable<List<Tourism>>
     fun setFavoriteTourism(tourism: Tourism, state: Boolean)
 }
