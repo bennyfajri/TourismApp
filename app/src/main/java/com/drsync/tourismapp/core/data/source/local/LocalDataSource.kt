34d05e7e9d38daad562850444c28369aefff7c84
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-07-04 17:59:45 +0700

    add : Koin

--- app/src/main/java/com/drsync/tourismapp/core/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/drsync/tourismapp/core/data/source/local/LocalDataSource.kt
@@ -4,22 +4,22 @@ import com.drsync.tourismapp.core.data.source.local.entity.TourismEntity
 import com.drsync.tourismapp.core.data.source.local.room.TourismDao
 import kotlinx.coroutines.flow.Flow
 
-class LocalDataSource private constructor(private val tourismDao: TourismDao) {
+class LocalDataSource(private val tourismDao: TourismDao) {
 
-    companion object {
-        private var instance: LocalDataSource? = null
-
-        fun getInstance(tourismDao: TourismDao): LocalDataSource =
-            instance ?: synchronized(this) {
-                instance ?: LocalDataSource(tourismDao)
-            }
-    }
+//    companion object {
+//        private var instance: LocalDataSource? = null
+//
+//        fun getInstance(tourismDao: TourismDao): LocalDataSource =
+//            instance ?: synchronized(this) {
+//                instance ?: LocalDataSource(tourismDao)
+//            }
+//    }
 
     fun getAllTourism(): Flow<List<TourismEntity>> = tourismDao.getAllTourism()
 
     fun getFavoriteTourism(): Flow<List<TourismEntity>> = tourismDao.getFavoriteTourism()
 
-    fun insertTourism(tourismList: List<TourismEntity>) = tourismDao.insertTourism(tourismList)
+    suspend fun insertTourism(tourismList: List<TourismEntity>) = tourismDao.insertTourism(tourismList)
 
     fun setFavoriteTourism(tourism: TourismEntity, newState: Boolean) {
         tourism.isFavorite = newState
